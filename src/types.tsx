fd5bc27a49a1233941edbb6441fbb1e10e27e0e3
da-okazaki <da-okazaki@outlook.com>
2021-08-13 19:50:49 +0900

    [modify] フォルダ階層を変更

---
+++ src/types.tsx
@@ -0,0 +1,28 @@
+// TabNavigation
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Message: undefined;
+  Notification: undefined;
+  Search: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type MessageParamList = {
+  MessageScreen: undefined;
+};
+
+export type NotificationParamList = {
+  NotificationScreen: undefined;
+};
+
+export type SearchParamList = {
+  SearchScreen: undefined;
+};
